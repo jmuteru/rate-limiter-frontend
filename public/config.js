67a77c6f1b6d0515e782874b6dea54d1447194d6
jmuteru <jeffmuteru@gmail.com>
2025-12-07 11:10:48 +0300

    fix

--- public/config.js
+++ public/config.js
@@ -1,6 +1,4 @@
-// Runtime configuration for the application
-// This file can be replaced at deployment time with the actual API URL
 window.__APP_CONFIG__ = window.__APP_CONFIG__ || {
-  apiUrl: 'https://rate-limiter-backend.onrender.com/api'
+  apiUrl: '/api'
 };
 
