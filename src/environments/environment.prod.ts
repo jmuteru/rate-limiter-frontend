143f3638bffd8dd6266611fee46f1bd7af14b701
jmuteru <jeffmuteru@gmail.com>
2025-12-07 11:38:48 +0300

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,16 +2,11 @@ function getApiUrl(): string {
   if ((window as any).__APP_CONFIG__?.apiUrl) {
     return (window as any).__APP_CONFIG__.apiUrl;
   }
-  
-  if ((window as any).__ENV__?.API_URL) {
-    return (window as any).__ENV__?.API_URL;
-  }
-  
   return 'https://rate-limiter-backend.onrender.com/api';
 }
 
 export const environment = {
   production: true,
-  apiUrl: '/api'
+  apiUrl: getApiUrl()
 };
 
